1c3ece5c3a0dea8ab1530730e3f3237e41081375
bashobbel <bashobbel@gmail.com>
2021-10-23 16:14:37 +0200

    Add project files.

---
+++ SelectfromCollectionview/SelectfromCollectionview/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SelectfromCollectionview
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+            UpdateSelectionData(Enumerable.Empty<Monkey>(), Enumerable.Empty<Monkey>());
+        }
+
+        void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSelectionData(e.PreviousSelection, e.CurrentSelection);
+        }
+
+        void UpdateSelectionData(IEnumerable<object> previousSelectedItems, IEnumerable<object> currentSelectedItems)
+        {
+            string previous = (previousSelectedItems.FirstOrDefault() as Monkey)?.Name;
+            string current = (currentSelectedItems.FirstOrDefault() as Monkey)?.Name;
+            currentSelectedItemLabel.Text = (currentSelectedItems.FirstOrDefault() as Monkey)?.Name;
+        }
+    }
+}
